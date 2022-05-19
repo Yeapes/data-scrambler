b16dbd653425b85dff576bc4971f5feedbc07816
Yeapes <yeapesh2831@gmail.com>
2022-05-19 22:04:05 +0600

    Encode & Decode function added

--- helpers.php
+++ helpers.php
@@ -1,5 +1,38 @@
 <?php
 
-function generateKey(){
-    
+function scrambleData($Originaldata, $key){
+    $originalKey = "abcdefghijklmnopqrstuvwxyz1234567890";
+    $data = '';
+    $length = strlen($Originaldata);
+
+    for($i = 0; $i < $length; $i++){
+        $currentChar = $Originaldata[$i];
+        $position = strpos($originalKey,$currentChar);
+
+        if($position !== false){
+             $data .= $key[$position];   
+        }else{
+            $data .= $currentChar;
+        }
+    }
+    return $data;
+}
+
+
+function decodeData($Originaldata, $key){
+    $originalKey = "abcdefghijklmnopqrstuvwxyz1234567890";
+    $data = '';
+    $length = strlen($Originaldata);
+
+    for($i = 0; $i < $length; $i++){
+        $currentChar = $Originaldata[$i];
+        $position = strpos($originalKey,$currentChar);
+
+        if($position !== false){
+             $data .= $originalKey[$position];   
+        }else{
+            $data .= $currentChar;
+        }
+    }
+    return $data;
 }
